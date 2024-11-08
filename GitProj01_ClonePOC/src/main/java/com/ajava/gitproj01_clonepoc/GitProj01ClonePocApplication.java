73d4a1055bf92474925329e7920d74e33c9befd4
Vinod Kumar <vinod.kumar@vitalityglobal.com>
2024-11-08 07:25:15 +0200

    First commit to Git project

---
+++ GitProj01_ClonePOC/src/main/java/com/ajava/gitproj01_clonepoc/GitProj01ClonePocApplication.java
@@ -0,0 +1,13 @@
+package com.ajava.gitproj01_clonepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProj01ClonePocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitProj01ClonePocApplication.class, args);
+    }
+
+}
---
+++ GitProj01_ClonePOC/src/test/java/com/ajava/gitproj01_clonepoc/GitProj01ClonePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajava.gitproj01_clonepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProj01ClonePocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
